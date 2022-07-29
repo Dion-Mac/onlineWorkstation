b13a43c48240a6c81465ccbbd1eca4c566794bb6
Dion-Mac <dionmac2016@gmail.com>
2022-07-30 06:54:38 +0800

    first commit

---
+++ src/main/java/com/frog/neural/neuralfrog/NeuralfrogApplication.java
@@ -0,0 +1,13 @@
+package com.frog.neural.neuralfrog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuralfrogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuralfrogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/frog/neural/neuralfrog/NeuralfrogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frog.neural.neuralfrog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuralfrogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
